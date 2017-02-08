ce1ef44f59c6cb20f0142a64b53529a8e5504c98
Iqbal Singh <isingh2@students.towson.edu>
2017-02-07 22:26:04 -0500

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.engine('html', require('ejs').renderFile);
 var helpers = require('express-helpers');
 helpers(app);
 
-var server = app.listen(8081, function () {
+var server = app.listen(5000, function () {
     console.log("server started")
 });
 
