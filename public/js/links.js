7b2db4629af6cf97ed66ee18e1b01e7b8293adea
iqbal-singh <isingh2@students.towson.edu>
2017-08-04 22:47:10 -0400

    link

--- public/js/links.js
+++ public/js/links.js
@@ -1,24 +1,26 @@
  
- var generateCopyURL = function(_id) {
+   var generateCopyURL = function(_id) {
     var songURL = window.location.protocol + '//' + window.location.host + '/files/' + _id;
 	var songURL_DOM_ID = '#' + _id + '-url';
     var songURLBox_DOM_ID = '#' + _id + '-urlbox';
-
-     $(document).ready(function(){
-     	$(songURL_DOM_ID).val(songURL);
+    
+    $(document).ready(function(){
+        $(songURL_DOM_ID).val(songURL);
      });
         
-        $(songURLBox_DOM_ID).click(function(){$(songURL_DOM_ID).focus();
+    $(songURLBox_DOM_ID).click(function(){
+        $(songURL_DOM_ID).focus();
+        document.execCommand("selectall");
     });
 
 
-        $(songURLBox_DOM_ID).mouseleave(function(){$(songURL_DOM_ID).blur();
+    $(songURLBox_DOM_ID).mouseleave(function(){
+        $(songURL_DOM_ID).blur();
     });
-}
+  }
 
-var OpenURL = function(_id) {
+  var OpenURL = function(_id) {
     var songURL = window.location.protocol + '//' + window.location.host + '/files/' + _id;
-
     window.open(songURL,"","width=600,height=300");
-}
+  }
 
